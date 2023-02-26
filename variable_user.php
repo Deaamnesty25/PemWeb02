551329e0de0c0de925a4d8ba22b45fa7885f0f97
Deaamnesty25 <114567873+Deaamnesty25@users.noreply.github.com>
2023-02-27 00:05:45 +0700

    Delete variable_user.php

--- variable_user.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nama = "Dea Amnesty";
-$prodi = "Sistem Informasi";
-$ipk = "3.8";
-
-echo "Nama saya adalah " . $nama . "<br>";
-echo "Program studi $prodi <br>";
-echo "ipk saya " . $ipk . ", Amin";
